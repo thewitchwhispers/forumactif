458be52ed708b09227ead6e7898502840392bd8d
thewitchwhispers <arceerpg@gmail.com>
2024-11-19 13:01:14 +0100

    Update mundeliasscript.js

--- mundeliasscript.js
+++ mundeliasscript.js
@@ -1,8 +1,13 @@
 window.onload = function() {
-    const logoutLink = document.querySelector('a#logout img[title="Déconnexion [ William Yaxley ]"]');
-    const mundeliasDiv = document.querySelector('.mundelias');
-
-    if (logoutLink && mundeliasDiv) {
-        mundeliasDiv.style.display = 'block';  // Rendre visible la div
+    // Sélectionner le lien de déconnexion
+    const logoutLink = document.querySelector('a#logout');
+    // Vérifier que l'élément existe et que les attributs alt et title sont corrects
+    if (logoutLink && logoutLink.querySelector('img[alt="Déconnexion [ William Yaxley ]"][title="Déconnexion [ William Yaxley ]"]')) {
+        // Sélectionner la div avec la classe .mundelias
+        const mundeliasDiv = document.querySelector('.mundelias');
+        // Rendre visible la div .mundelias si les conditions sont remplies
+        if (mundeliasDiv) {
+            mundeliasDiv.style.display = 'block';
+        }
     }
 };
