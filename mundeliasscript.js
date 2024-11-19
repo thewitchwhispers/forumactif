15fc093f818019a1f0a5097a968a90150993c6b7
thewitchwhispers <arceerpg@gmail.com>
2024-11-19 12:44:05 +0100

    Create mundeliasscript.js

---
+++ mundeliasscript.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+  if (document.querySelector('a#logout img[title="Déconnexion [ William Yaxley ]"]')) {
+    document.querySelector('.mundelias').style.display = 'block';
+  }
+};
